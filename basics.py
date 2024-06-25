bbcd43772aa7b074e4b3b10eef1d629e0783067a
Premanath Talamarla <premanath@syoft.com>
2024-06-25 22:39:51 +0530

    basics

--- basics.py
+++ basics.py
@@ -1,1 +1,40 @@
 # python Basics
+
+# Identifiers
+
+# variables
+# comment if
+#
+#
+# hello = 45
+# print("Hello ---> ", hello)
+# accpets alphabets
+# hello155 = 777
+# print(hello155)
+# hello8webjkhsjdkhfkjsdhjfkhsjksfhjksdhfjksdhfkjhsdkjfhsdkjhf = 777
+# print(hello8webjkhsjdkhfkjsdhjfkhsjksfhjksdhfjksdhfkjhsdkjfhsdkjhf)
+# __ ==> Private
+# _ ==> Protected
+# public
+# hello =777
+# print("Hello ---> ", hello)
+# print("id of hello ---> ", id(hello))
+# HELlO =8888
+# print("Hello ---> ", HELlO)
+# print("id of HELlO ---> ", id(HELlO))
+# hello = 9999
+# print("Hello2 ---> ", hello)
+# print("id of hello2 ---> ", id(hello))
+
+
+import keyword
+print("Keyword List ---> ", keyword.kwlist)
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break',
+#  'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for',
+#  'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or',
+#  'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+# Rules for identifiers
+# 1. Can not start with number
+# 2. Can not start with special characters
+# 3. Can not use keyword
