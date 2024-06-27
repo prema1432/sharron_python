118d1a2a25adf393fa7b8457a43c754fd34a72f4
Premanath Talamarla <premanath@syoft.com>
2024-06-27 22:52:54 +0530

    datatypes

---
+++ data_types.py
@@ -0,0 +1,125 @@
+# # Data types
+#
+# # int
+# a=23
+# print(a)
+# b=-34
+# print(b)
+#
+# print(type(a))
+#
+# print(type(b))
+#
+# # float
+# j=7.6
+# print(j)
+# print(type(j))
+#
+# # complex
+# k=7+8j
+#
+#
+# print(k)
+# print(type(k))
+#
+# # bool
+# a=True
+# b=False
+# print(a)
+# print(b)
+# print(type(a))
+# print(type(b))
+#
+#
+#
+# # str
+#
+#
+# h="Hyderabad"
+#
+#
+#
+# print(h)
+#
+# print(h[5])
+#
+# print(h[2:6])
+#
+# print(h[0:10:4])
+#
+#
+# print(h[-1])
+# print(h[-4:-2])
+#
+# print(len(h))
+#
+# print("hyderad --->",h*5)
+# h=h*5
+# print(len(h))
+
+
+# type casting
+a="23.76"
+
+print(a)
+print(type(a))
+
+b= float(a)
+
+print(b)
+print(type(b))
+
+
+c= str(a)
+print(c)
+print(type(c))
+
+d=int(a)
+
+print(d)
+print(type(d))
+
+# print(h[])
+
+
+# bytes
+# bytearray
+# tuple
+# range
+# list
+# set
+# frozenset
+# dict
+# None
+
+#
+# a = ''  # represent plain data
+# b = ""  #
+# c = """"""
+# d = '3243'
+#
+# # s='hello world I'm going to India
+# s = ("hello wrld I'm goin"
+#      ""
+#      ""
+#      ""
+#      ""
+#      ""
+#      ""
+#      " g to India")
+# print(s)
+#
+#
+# # Hey hi i am chitti
+# # how ru
+# #bye
+#
+# k="""Hey Hi I am chitti
+#
+# I am python developer
+#
+# and AI Developer ..... Thank uou                uuu
+# """
+#
+#
+# print(k)
