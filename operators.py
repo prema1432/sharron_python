21c6c76188588db58ff486487fb25cc458613686
Premanath Talamarla <premanath@syoft.com>
2024-07-11 22:56:50 +0530

    operators code added

---
+++ operators.py
@@ -0,0 +1,106 @@
+# # # operator
+# # # symbol to perfom actiuons
+# # # Arithmetic Operations
+# # # Relationa Operators (Comparision)
+# # # Logical Opoerators
+# # #Bitwise Operators
+# # # Assignment Operators
+# # # Special Operators
+
+
+
+# # # Airthmatic Operations
+# # # +,-,*,/,%,//,**
+
+# # # a=10
+# # # b=20
+# # # print(f"a+b =",a+b)
+# # # c=a-b
+# # # print(f"a-b = ",c)
+# # # print(f" a x b", a*b)
+# # # print(f" a % b",a %b)
+# # # print(f"a // b =",a//b)
+# # # print(f"a ** b",a**b)
+
+
+# # #2.Relational Operators
+# # # >,>=,<,<=
+# # a=100
+# # b=100
+
+# # print(f"a  > b", a>b)
+# # if a <= b:
+# #     print("a is greater than b")
+# # else:
+# #     print(" a is lestthan b")
+
+# # #3.Equality opearors
+# # # === , !=
+
+# # a=231
+# # b=23
+
+# # print(f"a ---- and b is = ",a == b )
+# # if a != b:
+# #     print("a and b are same")
+# # else:
+# #     print("a and b are not same")
+
+
+# # #4. logical operators
+
+# # # and, or, not 
+
+# # a =34
+# # b=45
+# # c=50
+# # d=78
+
+# # if a == b and a == c or  a != d:
+# #     print(" a and b is equal")
+
+
+# # # True and True or False
+# # # 1 or 0
+# # # 1
+# # # if not b:
+
+
+# #5. bitwise operators
+
+# # &,|,^,~,>>,<<
+# a = 45
+# b=67
+# if a & b  :
+#     print("jhkh")
+
+# if a | b :
+#     print("hgjghgjg")
+
+# #6.Assignment OPerators
+
+# # +=,-=,*=,&=
+
+# a = 10
+
+# a = a +10
+# print("a is the value of :",a)
+
+# a-=2
+# print("a is new updated value ", a)
+
+# # Ternarory operator - Conditional operatorr
+
+
+# a=322
+# b=77
+# if a <b:
+#     print("yyyyy")
+# else:
+#     print(
+#         "yyes"
+#     )
+
+
+# j= 78 if a<b else 90
+# print("j value",j)
